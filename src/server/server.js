8fb1d94df0ba35736ff0546d350fc6fc1d147e11
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-11-24 16:40:21 -0700

    seed mongolab database on heroku

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,6 @@
 var debug = require('debug')('passport-mongo'),
     app = require('./app');
 
-process.env.NODE_ENV = 'development';
 app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), function() {
