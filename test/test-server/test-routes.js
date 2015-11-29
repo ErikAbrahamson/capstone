065186ccf8c5471d7d522af73bd03f3a58e8684c
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-11-29 10:40:02 -0700

    update tests

--- test/test-server/test-routes.js
+++ test/test-server/test-routes.js
@@ -4,7 +4,8 @@ var mocha = require('mocha');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../../src/server/app.js');
-var Task = require('../../src/server/models/Task');
+var Task = require('../../src/server/models/task.js');
+var User = require('../../src/server/models/user.js');
 
 var should = chai.should();
 chai.use(chaiHttp);
@@ -13,17 +14,10 @@ describe('Task', function() {
 
   Task.collection.drop();
   var date = new Date();
-  date.setDate(date.getDate() + 10 /*days*/);
+  date.setDate(date.getDate() + 10);
 
   beforeEach(function(done) {
 
-    var newUser = new User({
-      title: 'test@test.com',
-      password: 'test',
-      phone: '123-456-7890',
-      twitter: '@user',
-      tasks:[newTask]
-    });
     var newTask = new Task({
       title: 'Finish Tests',
       description: 'I\'d better finish these tests',
@@ -48,10 +42,19 @@ describe('Task', function() {
         twitter_post: true
       }
     });
+    var newUser = new User({
+      username: 'test@test.com',
+      password: 'test',
+      phone: '123-456-7890',
+      twitter: '@user',
+      tasks:[newTask, secondTask]
+    });
 
-    newTask.save(function(error) { done(); });
-    secondTask.save(function(error) { done(); });
-    newUser.save(function(error) { done(); });
+    newTask.save();
+    secondTask.save();
+    newUser.save();
+    done();
+
   });
   afterEach(function(done) {
     User.collection.drop();
@@ -59,16 +62,55 @@ describe('Task', function() {
     done();
   });
 
+  // get all users
+  it('Should return all users and associated tasks', function(done) {
+    chai.request(server)
+      .get('/users/')
+      .end(function(error, res) {
+        res.should.have.status(200);
+        res.body[0].should.have.property('_id');
+        res.body[0].tasks.length.should.equal(2);
+        res.body[0].should.have.property('username');
+        res.body[0].should.have.property('tasks');
+        res.body[0].should.have.property('twitter');
+        res.body[0].twitter.should.equal('@user');
+        done();
+      });
+    });
+  // get one user
+  it('Should return a single user', function(done) {
+    var newUser = new User({
+      username: 'Rick',
+      password: '12345',
+      phone: '123-456-7890',
+      twitter: '@username',
+      tasks: []
+    });
+    newUser.save(function(error, data) {
+      chai.request(server)
+        .get('/user/' + data.id)
+        .end(function(error, res) {
+          res.should.have.status(200);
+          res.body.should.have.property('_id');
+          res.body.should.have.property('username');
+          res.body.should.have.property('password');
+          res.body.should.have.property('twitter');
+          res.body.tasks.length.should.equal(0);
+          res.body.twitter.should.equal('@username');
+          done();
+        });
+    });
+
   //get all Task samples
   it('Should return all tasks for a user', function(done) {
 
-    var id = response.body[0]._id,
-    update = { $pushAll : { tasks : [newTask, secondTask] }},
-    options = { new:true };
+    // var id = response.body[0]._id,
+    // update = { $pushAll : { tasks : [newTask, secondTask] }},
+    // options = { new:true };
 
     chai.request(server)
-      .get('/user/' + id + '/tasks')
-      .end(function(error, res){
+      .get('/user/' + res.body[0]._id + '/tasks')
+      .end(function(erroror, res) {
         res.should.have.status(200);
         res.body.success.should.be.a('array');
         res.body.success.length.should.equal(2);
@@ -78,7 +120,8 @@ describe('Task', function() {
         res.body.success[1].should.have.property('priority');
         done();
       });
+
+
     });
-
-
+  });
 });
