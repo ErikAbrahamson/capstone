63e8797cd525aebe0777569a0af69b3493231344
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-12-01 10:10:52 -0700

    Fix async issues in task test suite

--- test/test-server/test-tasks.js
+++ test/test-server/test-tasks.js
@@ -20,6 +20,27 @@ describe('User Tasks', function() {
     var date = new Date();
     date = date.setDate(15);
 
+    var newUser = new User({
+      username: 'test@test.com',
+      password: 'test',
+      phone: '123-456-7890',
+      twitter: '@user',
+      tasks:[]
+    });
+
+    newUser.save();
+    done();
+  });
+
+  afterEach(function(done) {
+    User.collection.drop();
+    Task.collection.drop();
+    done();
+  });
+
+  it('Should return all tasks for a user', function(done) {
+    var date = new Date();
+    date = date.setDate(15);
     var newTask = new Task({
       title: 'Finish Tests',
       description: 'I\'d better finish these tests',
@@ -46,37 +67,16 @@ describe('User Tasks', function() {
       }
     });
     secondTask.save();
-    var newUser = new User({
-      username: 'test@test.com',
-      password: 'test',
-      phone: '123-456-7890',
-      twitter: '@user',
-      tasks:[]
-    });
-
-    newUser.save();
-
-    // Workaround for async issues
-    var userID = newUser._id, isNew = { new:true },
-      pushTasks = { $pushAll : {tasks : [newTask, secondTask] }};
-
-    User.findByIdAndUpdateQ(userID, pushTasks, isNew)
-      .then(function(result) { done(); })
-      .catch(function(error) { done(); });
-  });
-
-
-  afterEach(function(done) {
-    User.collection.drop();
-    Task.collection.drop();
-    done();
-  });
-
-  it('Should return all tasks for a user', function(done) {
-    var date = new Date();
-    date = date.setDate(15);
     chai.request(server).get('/users/')
       .end(function(err, res) {
+
+        var userID = res.body[0]._id, isNew = { new:true },
+          pushTasks = { $pushAll : {tasks : [newTask, secondTask] }};
+
+        User.findByIdAndUpdateQ(userID, pushTasks, isNew)
+          .then(function(result) {})
+          .catch(function(error) {});
+
         chai.request(server)
           .get('/user/' + res.body[0]._id + '/tasks/')
           .end(function(error, res) {
@@ -112,26 +112,65 @@ describe('User Tasks', function() {
   });
 
   it('Should return a single task from a user', function(done) {
-    chai.request(server)
-      .get('/users/').end(function(error, res) {
+    var date = new Date();
+    date = date.setDate(15);
+    var newTask = new Task({
+      title: 'Finish Tests',
+      description: 'I\'d better finish these tests',
+      deadline: date,
+      priority: 10,
+      complete: false,
+      severity: 10,
+      punishment_type : {
+        donation: true,
+        twitter_post: false
+      }
+    });
+    newTask.save();
+    var secondTask = new Task({
+      title: 'Seriously, finish tests',
+      description: 'I\'d better finish these damn tests',
+      deadline: date,
+      priority: 10,
+      complete: false,
+      severity: 10,
+      punishment_type : {
+        donation: false,
+        twitter_post: true
+      }
+    });
+    secondTask.save();
+    chai.request(server).get('/users/')
+      .end(function(err, res) {
+
+        var userID = res.body[0]._id, isNew = { new:true },
+          pushTasks = { $pushAll : {tasks : [newTask, secondTask] }};
+
+        User.findByIdAndUpdateQ(userID, pushTasks, isNew)
+          .then(function(result) {})
+          .catch(function(error) {});
+
         chai.request(server)
-          .get('/user/' + res.body[0]._id + '/task/' + res.body[0].tasks[0])
-          .end(function(error, res) {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('_id');
-            res.body.should.have.property('title');
-            res.body.should.have.property('description');
-            res.body.should.have.property('deadline');
-            res.body.should.have.property('priority');
-            res.body.should.have.property('complete');
-            res.body.should.have.property('severity');
-            res.body.should.have.property('punishment_type');
-            res.body.punishment_type.should.have.property('donation');
-            res.body.punishment_type.should.have.property('twitter_post');
-            res.body.title.should.equal('Finish Tests');
-            res.body.priority.should.equal(10);
-            done();
+          .get('/users/').end(function(error, res) {
+            chai.request(server)
+              .get('/user/' + res.body[0]._id + '/task/' + res.body[0].tasks[0])
+              .end(function(error, res) {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('_id');
+                res.body.should.have.property('title');
+                res.body.should.have.property('description');
+                res.body.should.have.property('deadline');
+                res.body.should.have.property('priority');
+                res.body.should.have.property('complete');
+                res.body.should.have.property('severity');
+                res.body.should.have.property('punishment_type');
+                res.body.punishment_type.should.have.property('donation');
+                res.body.punishment_type.should.have.property('twitter_post');
+                res.body.title.should.equal('Finish Tests');
+                res.body.priority.should.equal(10);
+                done();
+              });
           });
       });
   });
@@ -156,15 +195,38 @@ describe('User Tasks', function() {
           })
           .end(function(err, res) {
             res.should.have.status(200);
-            res.body.tasks.length.should.equal(3);
+            res.body.tasks.length.should.equal(1);
             done();
           });
       });
   });
 
   it('Should allow a user to edit tasks', function(done) {
+    var date = new Date();
+    date = date.setDate(15);
+    var newTask = new Task({
+      title: 'Finish Tests',
+      description: 'I\'d better finish these tests',
+      deadline: date,
+      priority: 10,
+      complete: false,
+      severity: 10,
+      punishment_type : {
+        donation: true,
+        twitter_post: false
+      }
+    });
+    newTask.save();
     chai.request(server)
       .get('/users/').end(function(err, res) {
+
+        var userID = res.body[0]._id, isNew = { new:true },
+          pushTasks = { $push: {tasks : [newTask] }};
+
+        User.findByIdAndUpdateQ(userID, pushTasks, isNew)
+          .then(function(result) {})
+          .catch(function(error) {});
+
         chai.request(server)
           .put('/user/' + res.body[0]._id + '/task/' + res.body[0].tasks[0])
           .send({
@@ -182,8 +244,31 @@ describe('User Tasks', function() {
   });
 
   it('Should allow a user to remove a task', function(done) {
+    var date = new Date();
+    date = date.setDate(15);
+    var newTask = new Task({
+      title: 'Finish Tests',
+      description: 'I\'d better finish these tests',
+      deadline: date,
+      priority: 10,
+      complete: false,
+      severity: 10,
+      punishment_type : {
+        donation: true,
+        twitter_post: false
+      }
+    });
+    newTask.save();
     chai.request(server)
       .get('/users/').end(function(err, res) {
+
+        var userID = res.body[0]._id, isNew = { new:true },
+          pushTasks = { $push: {tasks : [newTask] }};
+
+        User.findByIdAndUpdateQ(userID, pushTasks, isNew)
+          .then(function(result) { done(); })
+          .catch(function(error) { done(); });
+
         chai.request(server)
           .delete('/user/' + res.body[0]._id + '/task/' + res.body[0].tasks[0])
           .end(function(error, res) {
