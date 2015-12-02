8c366c4d00d6ffdb7d5cb26506fd511725b20e0a
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-12-02 16:46:18 -0700

    FIX subdoc pushing issue

--- src/server/routes/tasks.js
+++ src/server/routes/tasks.js
@@ -14,15 +14,29 @@ router.get('/user/:id/tasks', function(req, res, next) {
 });
 
 router.post('/user/:id/task', function(req, res, next) {
-  var newTask = new Task(req.body);
-    newTask.saveQ();
-  var update = { $push: {tasks : newTask }}, options = {
-    new: true,
-    upsert: true
-  }, id = req.params.id;
-  User.findByIdAndUpdateQ(id, update, options)
-    .then(function(result) { res.json(result); })
-    .catch(function(error) { res.send(error); });
+  var newTask = new Task({
+    title: req.body.title,
+    description: req.body.description,
+    deadline: req.body.deadline,
+    priority: req.body.priority,
+    complete: req.body.complete,
+    severity: req.body.severity,
+    punishment_type: {
+      donation: req.body.donation,
+      twitter_post: req.body.twitter_post
+    }
+  });
+    newTask.saveQ()
+      .then(function(result) {
+        var update = { $push : {tasks : newTask }}, options = {
+          new: true,
+          upsert: true
+        }, id = req.params.id;
+        User.findByIdAndUpdateQ(id, update, options)
+          .then(function(result) { res.json(result); })
+          .catch(function(error) { res.send(error); });
+
+      });
 });
 
 router.get('/user/:userid/task/:taskid', function(req, res, next) {
