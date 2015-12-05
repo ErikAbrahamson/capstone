352c1e080882ed9fc783fd690e3ec69077e934c8
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-12-05 10:44:34 -0700

    remove bower because it is bower

--- src/client/controllers/grid.js
+++ src/client/controllers/grid.js
@@ -5,19 +5,31 @@
     ['$scope', '$rootScope','$mdToast', '$location', 'AuthService',
     function ($scope, $rootScope, $mdToast, $location, AuthService) {
 
-      $scope.tileColors = [];
-
-      $scope.colorTiles = function() {
-        var tiles = [];
-        for (var i = 0; i < $rootScope.currentUser.tasks; i++) {
-          tiles.push({
-            // color: randomColor(),
-            colspan: randomSpan(),
-            rowspan: randomSpan()
-          });
+      var COLORS = [];
+        this.colorTiles = (function() {
+          var tiles = [];
+          for (var i = 0; i < 46; i++) {
+            tiles.push({
+              color: randomColor(),
+              colspan: randomSpan(),
+              rowspan: randomSpan()
+            });
+          }
+          return tiles;
+        })();
+        function randomColor() {
+          return COLORS[Math.floor(Math.random() * COLORS.length)];
         }
-        return tiles;
-      };
+        function randomSpan() {
+          var r = Math.random();
+          if (r < 0.8) {
+            return 1;
+          } else if (r < 0.9) {
+            return 2;
+          } else {
+            return 3;
+          }
+        }
 
     }]);
 }());
